c7180346948e28426596e8748967829d9f6ff8fe
María Pérez <114663801+perezrmaria@users.noreply.github.com>
2023-02-19 12:25:07 +0100

    Update appAMPLab_Maria.py

--- appAMPLab_Maria.py
+++ appAMPLab_Maria.py
@@ -18,6 +18,28 @@ audio_analysis = load_essentia_analysis()
 audio_analysis_styles = audio_analysis.columns
 st.write('Loaded audio analysis for', len(audio_analysis), 'tracks.')
 
+st.write('## ⌛️ Tempo')
+tempo = st.slider('Choose a tempo', 60, 180, 25)
+st.write("The following tempo is selected:", tempo)
+
+st.write('## 🪗 Type of song: instrumental or vocal tune')
+instrument = st.checkbox('🎙️ 🎻 Instrumental or vocal tune')
+
+if instrument:
+    st.write('Instrumental song is selected!🎺')
+if not instrument:
+    st.erite('Vocal song is selected! 🎤')
+    
+st.write('## 🩰 Danceability')
+danceability = st.slider('Choose how danceable you want the song to be:', 0, 3)
+st.write("Danceability value is...💃🏽", danceability)
+
+st.write('## 🤨🤩 Arousal and valence')
+arousal = st.slider('Choose the arousal:', 1,9)
+st.write("🐢 Arousal value is...", arousal)
+valence = st.slider('Choose the valence:', 1,9)
+st.write("🥹 Valence value is...", valence)
+
 st.write('## 🔍 Select')
 st.write('### By style')
 st.write('Style activation statistics:')
